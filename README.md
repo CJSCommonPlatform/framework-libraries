3e93eb3448bca0d071525f55b38660de1831a1db
Pradeep Thomas <pradeep.thomas@hmcts.net>
2016-10-03 13:36:28 +0100

    Add Artemis Server

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# embedded-artemis" 
+# embedded-artemis
+
+# Usage
+
+An embedded version of Artemis that can be included in a project for testing. 
+
+Supply the broker.xml in the main/resource or the test/resource folder.
---
+++ src/main/java/uk/gov/justice/artemis/EmbeddedArtemisServer.java
@@ -0,0 +1,71 @@
+package uk.gov.justice.artemis;
+
+import java.util.Set;
+
+import org.apache.activemq.artemis.core.security.CheckType;
+import org.apache.activemq.artemis.core.security.Role;
+import org.apache.activemq.artemis.jms.server.embedded.EmbeddedJMS;
+import org.apache.activemq.artemis.spi.core.security.ActiveMQSecurityManager;
+
+/**
+ * 
+ * An embedded Artemis server that can be used for testing
+ *
+ */
+public class EmbeddedArtemisServer {
+
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            public void run() {
+                try {
+                    stopServer();
+                } catch (Exception e) {
+                }
+            }
+        }));
+    }
+
+    private static EmbeddedJMS jmsServer;
+    
+    private EmbeddedArtemisServer() {}
+
+    public static void main(String args[]) throws Exception {
+
+        startServer();        
+    }
+    
+    public static final void startServer() throws Exception {
+        
+        //ensure that the loggers are in the classpath and loaded
+        Class.forName(org.apache.activemq.artemis.core.server.ActiveMQServerLogger.class
+                        .getCanonicalName());
+        Class.forName(org.apache.activemq.artemis.core.server.ActiveMQServerLogger_$logger.class
+                        .getCanonicalName());
+
+        final EmbeddedJMS jmsServer = new EmbeddedJMS();
+        
+        jmsServer.setConfigResourcePath("broker.xml");
+        
+        //override the security manager to cater for multiple test configurations
+        // users and passwords
+        jmsServer.setSecurityManager(new ActiveMQSecurityManager() {
+            @Override
+            public boolean validateUser(String user, String password) {
+                return true;
+            }
+
+            @Override
+            public boolean validateUserAndRole(String user, String password, Set<Role> roles,
+                            CheckType checkType) {
+                return true;
+            }
+        });
+        jmsServer.start();
+    }
+
+    public static final void stopServer() throws Exception {
+        if (jmsServer != null) {
+            jmsServer.stop();
+        }
+    }
+}

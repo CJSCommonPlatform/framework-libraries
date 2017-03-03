fb858d263af509533a6b93a39624b9c8da9d64c9
amckenzie <allan.mckenzie@agilesphere.co.uk>
2017-03-03 12:18:36 +0000

    Add java docs

--- utilities-core/src/main/java/uk/gov/justice/services/jdbc/persistence/InitialContextFactory.java
+++ utilities-core/src/main/java/uk/gov/justice/services/jdbc/persistence/InitialContextFactory.java
@@ -3,8 +3,17 @@ package uk.gov.justice.services.jdbc.persistence;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+/**
+ * Simple wrapper class for getting an {@link InitialContext} to allow for mocking in tests
+ */
 public class InitialContextFactory {
 
+    /**
+     * Instantiates a new {@link InitialContext}
+     *
+     * @return a shiny new {@link InitialContext}
+     * @throws NamingException if instantiating the {@link InitialContext} fails
+     */
     public InitialContext create() throws NamingException {
         return new InitialContext();
     }

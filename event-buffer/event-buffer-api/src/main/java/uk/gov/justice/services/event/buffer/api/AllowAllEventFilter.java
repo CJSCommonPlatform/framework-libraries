9c787e1137f0a89f84c2032a50247a4c4add7a23
Jacek Obarymski <jacek.obarymski@agilesphere.co.uk>
2017-04-07 16:02:29 +0100

    Move event filter to event buffer module

---
+++ event-buffer/event-buffer-api/src/main/java/uk/gov/justice/services/event/buffer/api/AllowAllEventFilter.java
@@ -0,0 +1,17 @@
+package uk.gov.justice.services.event.buffer.api;
+
+import uk.gov.justice.services.event.buffer.api.EventFilter;
+
+import javax.annotation.Priority;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
+
+@ApplicationScoped
+@Alternative
+@Priority(1)
+public class AllowAllEventFilter implements EventFilter {
+    @Override
+    public boolean accepts(final String eventName) {
+        return true;
+    }
+}

aaf8ff1bd97a1a0a6ea10a7e2f6f5ddb68f40edc
amckenzie <allan.mckenzie@agilesphere.co.uk>
2017-08-21 11:16:34 +0100

    add support for enums for ObjectMapper and ObjectMapperProducer

--- utilities-core/src/main/java/uk/gov/justice/services/common/converter/jackson/ObjectMapperProducer.java
+++ utilities-core/src/main/java/uk/gov/justice/services/common/converter/jackson/ObjectMapperProducer.java
@@ -2,8 +2,10 @@ package uk.gov.justice.services.common.converter.jackson;
 
 import static com.fasterxml.jackson.annotation.JsonCreator.Mode.PROPERTIES;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_ABSENT;
+import static com.fasterxml.jackson.databind.DeserializationFeature.READ_ENUMS_USING_TO_STRING;
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_WITH_ZONE_ID;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_ENUMS_USING_TO_STRING;
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_NULL_MAP_VALUES;
 import static java.time.ZoneOffset.UTC;
 import static java.time.format.DateTimeFormatter.ofPattern;
@@ -40,7 +42,9 @@ public class ObjectMapperProducer {
                 .configure(WRITE_DATES_WITH_ZONE_ID, false)
                 .configure(WRITE_NULL_MAP_VALUES, false)
                 .setTimeZone(getTimeZone(UTC))
-                .setSerializationInclusion(NON_ABSENT);
+                .setSerializationInclusion(NON_ABSENT)
+                .enable(WRITE_ENUMS_USING_TO_STRING)
+                .enable(READ_ENUMS_USING_TO_STRING);
     }
 
     private JavaTimeModule javaTimeModuleWithFormattedDateTime() {

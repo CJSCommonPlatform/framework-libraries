2e45d231119466063080e09ff16179fbc8cc9b32
Shaun Francis <shaun@mapingo.co.uk>
2018-01-17 13:58:03 +0000

    Remove components and add interceptors modules

---
+++ interceptors/event-listener-interceptors/src/test/java/uk/gov/justice/services/components/event/listener/interceptors/it/util/repository/StreamBufferOpenEjbAwareJdbcRepository.java
@@ -0,0 +1,12 @@
+package uk.gov.justice.services.components.event.listener.interceptors.it.util.repository;
+
+import uk.gov.justice.services.event.buffer.core.repository.streambuffer.StreamBufferJdbcRepository;
+
+
+/**
+ * @deprecated No longer used in the Framework and will be removed at a later date. Use {@link StreamBufferJdbcRepository} instead
+ */
+@Deprecated
+public class StreamBufferOpenEjbAwareJdbcRepository extends StreamBufferJdbcRepository {
+
+}

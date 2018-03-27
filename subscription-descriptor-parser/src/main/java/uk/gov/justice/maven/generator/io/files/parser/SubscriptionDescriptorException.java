227c29ff54825d5a52ae72e4804b8b8463d358bc
amckenzie <allan.mckenzie@agilesphere.co.uk>
2018-03-27 12:40:45 +0100

    Move Subscription objects to Framework Event Sourcing

--- subscription-descriptor-parser/src/main/java/uk/gov/justice/maven/generator/io/files/parser/SubscriptionDescriptorException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.maven.generator.io.files.parser;
-
-public class SubscriptionDescriptorException extends RuntimeException {
-    public SubscriptionDescriptorException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-}

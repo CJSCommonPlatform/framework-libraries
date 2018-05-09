cabd899484dfdec91e6bdb567026dc88e08d2538
Martin Spasov <martin.y.spasov@gmail.com>
2018-05-09 11:48:12 +0100

    Remove cloneStream and clearStream from EventSource.

--- framework-api-event-source/src/main/java/uk/gov/justice/services/eventsourcing/source/core/EventSource.java
+++ framework-api-event-source/src/main/java/uk/gov/justice/services/eventsourcing/source/core/EventSource.java
@@ -1,7 +1,5 @@
 package uk.gov.justice.services.eventsourcing.source.core;
 
-import uk.gov.justice.services.eventsourcing.source.core.exception.EventStreamException;
-
 import java.util.UUID;
 import java.util.stream.Stream;
 
@@ -32,20 +30,4 @@ public interface EventSource {
      * @return a stream of {@link EventStream}s
      */
     Stream<EventStream> getStreamsFrom(final long position);
-
-    /**
-     * Clones the stream into a new stream id.
-     *
-     * @param streamId the id of the stream to be cloned
-     * @return the id of the clone
-     * @throws EventStreamException if the cloning of the stream fails
-     */
-    UUID cloneStream(final UUID streamId) throws EventStreamException;
-
-    /**
-     * Clears all of the events from a stream id.
-     *
-     * @param streamId the id of the stream to be cleared
-     */
-    void clearStream(final UUID streamId) throws EventStreamException;
 }

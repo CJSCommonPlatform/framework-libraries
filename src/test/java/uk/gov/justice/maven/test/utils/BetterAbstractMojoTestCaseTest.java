80eb1b45ba04e7cdcbb576860adb1ad6e960761c
Mahesh Subramanian <mahesh.subramanian@hmcts.net>
2019-02-12 11:06:00 +0000

    Fix for travis configuration

---
+++ src/test/java/uk/gov/justice/maven/test/utils/BetterAbstractMojoTestCaseTest.java
@@ -0,0 +1,27 @@
+package uk.gov.justice.maven.test.utils;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.maven.plugin.Mojo;
+import org.junit.Test;
+
+public class BetterAbstractMojoTestCaseTest {
+
+    private BetterAbstractMojoTestCase mojoTestCase = new BetterAbstractMojoTestCase() {
+        @Override
+        protected Mojo lookupConfiguredMojo(final File pom, final String goal) throws Exception {
+            return super.lookupConfiguredMojo(pom, goal);
+        }
+    };
+
+    /**
+     * Adding this test for coveralls report generation
+     */
+
+    @Test
+    public void emptyAssertion() {
+        assertTrue(true);
+    }
+}

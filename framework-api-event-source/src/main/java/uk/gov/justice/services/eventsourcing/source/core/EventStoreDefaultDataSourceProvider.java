260bb71dbf1b78d698264f4f79e9c683ba69da57
amckenzie <allan.mckenzie@agilesphere.co.uk>
2019-04-24 10:31:08 +0100

    Add alternative get DataSource method to event store DataSource provider

--- framework-api-event-source/src/main/java/uk/gov/justice/services/eventsourcing/source/core/EventStoreDefaultDataSourceProvider.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.services.eventsourcing.source.core;
-
-import javax.sql.DataSource;
-
-public interface EventStoreDefaultDataSourceProvider {
-
-    DataSource getDataSource();
-}

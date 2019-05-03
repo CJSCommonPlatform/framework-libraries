362b10ab0859655d483d328b50006e9a5556e17f
Mohamed Farouk <mohamed.farouk@hmcts.net>
2019-05-03 16:19:00 +0100

    remove validation from library

--- json-transformer-jolt/src/main/java/uk/gov/justice/json/jolt/TransformationException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.json.jolt;
-
-public class TransformationException extends RuntimeException {
-
-    public TransformationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}

e2a66faad66d48b5581e55d2cf2ff3ec55478bad
allanmckenzie <allan.mckenzie@hmcts.net>
2023-06-26 15:46:09 +0100

    fix stuff

--- framework-utilities/utilities/utilities-core/src/main/java/uk/gov/justice/services/common/configuration/GlobalValueProducer.java
+++ framework-utilities/utilities/utilities-core/src/main/java/uk/gov/justice/services/common/configuration/GlobalValueProducer.java
@@ -2,10 +2,11 @@ package uk.gov.justice.services.common.configuration;
 
 import static uk.gov.justice.services.common.configuration.CommonValueAnnotationDef.globalValueAnnotationOf;
 
+import javax.naming.NamingException;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Produces;
 import jakarta.enterprise.inject.spi.InjectionPoint;
-import jakarta.naming.NamingException;
 
 /**
  * Looks up global jndi names in order to inject their values into @GlobalValue annotated
